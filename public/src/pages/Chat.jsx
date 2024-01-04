87386b9182c7310bf088a3e03435f05ec60aa806
Deblina-Mandal <mandaldeblina12@gmail.com>
2024-01-04 20:58:17 +0530

    Chatcontainer setup

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,37 +1,60 @@
-import React,{ userState, useEffect } from 'react'
+import React, { userState, useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
+
+
 function Chat() {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
   
+  useEffect(async () => {
+    if (!localStorage.getItem("chat-app-user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+    }
+  }, []);
+  // useEffect(async () => {
+  //   if (currentUser) {
+  //     if (currentUser.isAvatarImageSet) {
+  //       const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+  //       setContacts(data.data);
+  //     } else {
+  //       navigate("/setAvatar");
+  //     }
+  //   }
+  // }, [currentUser]);
   
   return (
     <Container>
       <div className="container">
-
+        <Contacts contacts={contacts} />
       </div>
     </Container>
-  )
+  );
 }
 
-const Container =styled.div`
-height:100vh;
-width:100vw;
-display: flex;
-flex-direction:column;
-justify-content: center;
-gap:1rem;
-align-items: center;
-.container{
-  height: 90vh;
-  width:95vw;
-  background-color: red;
-  display:grid;
-  grid-template-columns: 25% 75% ;
-  @media screen and (min-width:720px) and (max-width:1080px)
-  {
-    grid-template-columns: 35% 65%;
-
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  .container {
+    height: 90vh;
+    width: 95vw;
+    background-color: #d3cbcb;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
   }
-}
 `;
-export default Chat
+export default Chat;
